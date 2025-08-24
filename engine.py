8b683e72f25e64500956212b341f90aa86ecd761
Mathew <mathewkariuki2005@gmail.com>
2025-08-24 12:15:39 +0300

    created ten tables

---
+++ engine.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from dotenv import load_dotenv
+from sqlalchemy.pool import NullPool
+
+#this loads the variables from the .env file
+load_dotenv()
+
+db_credentials={
+    "drivername": "postgresql+psycopg2",
+    "username": os.getenv("DB_USERNAME"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "database": os.getenv("DB_NAME"),
+}
+
+DATABASE_URL=URL.create(**db_credentials)
+
+engine=create_engine(DATABASE_URL,echo=True, poolclass=NullPool)
+print(f"succes,the credentials are:{db_credentials}")
