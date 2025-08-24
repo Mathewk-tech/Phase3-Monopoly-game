f745d24af3a8a6b10c0d6c3f9b582063be46983d
Mathew <mathewkariuki2005@gmail.com>
2025-08-24 14:26:41 +0300

    added add data.py

--- data.py
+++ data.py
@@ -1,86 +1,79 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship,sessionmaker
 from engine import engine
-
-Base = declarative_base()
-
-
-class Player(Base):
-    __tablename__ = "players"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    money = Column(Integer, default=1500)
-
-    properties = relationship("Property", back_populates="owner")
-    turns = relationship("Turn", back_populates="player")
-    transactions = relationship("Transaction", back_populates="player")
-
-class Property(Base):
-    __tablename__ = "properties"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    price = Column(Integer, nullable=False)
-    owner_id = Column(Integer, ForeignKey("players.id"))
-
-    owner = relationship("Player", back_populates="properties")
+from tables import Player,Property,Game,Card,ChanceCard,CommunityChestCard
 
 
-class Game(Base):
-    __tablename__ = "games"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    active = Column(Boolean, default=True)
+session=sessionmaker(bind=engine)
+session=session()
 
-    turns = relationship("Turn", back_populates="game")
+def add_player():
+    name=input("Enter player name: ")
+    new_player=Player(name=name,money=1500)
+    session.add(new_player)
+    session.commit()
+    print(f"Added {name} successfully")
+
+    
+def add_property():
+    name=input("Enter player name: ")
+    price=input("Enter price: ")
+    new_property=property(name=name,price=price)
+    session.add(new_property)
+    session.commit()
+    print(f"Added {name} successfully")
+
+def add_card():
+    name=input("Enter player name: ")
+    new_card=Card(name=name)
+    session.add(new_card)
+    session.commit()
+    print(f"Added {name} successfully")
+
+def add_chancecard():
+    name=input("Enter chance name: ")
+    new_chance=ChanceCard(name=name)
+    session.add(new_chance)
+    session.commit()
+    print(f"Added {name} successfully")
+
+def add_communitychestcard():
+    name=input("Enter player name: ")
+    new_card=CommunityChestCard(name=name)
+    session.add(new_card)
+    session.commit()
+    print(f"Added {name} successfully")
 
 
-
-class Card(Base):
-    __tablename__ = "cards"
-    id = Column(Integer, primary_key=True)
-    description = Column(String)
-
-class ChanceCard(Base):
-    __tablename__ = "chance_cards"
-    id = Column(Integer, primary_key=True)
-    description = Column(String)
-
-class CommunityChestCard(Base):
-    __tablename__ = "community_chest_cards"
-    id = Column(Integer, primary_key=True)
-    description = Column(String)
-
-class Transaction(Base):
-    __tablename__ = "transactions"
-    id = Column(Integer, primary_key=True)
-    player_id = Column(Integer, ForeignKey("players.id"))
-    amount = Column(Integer)
-
-    player = relationship("Player", back_populates="transactions")
-
-class Turn(Base):
-    __tablename__ = "turns"
-    id = Column(Integer, primary_key=True)
-    game_id = Column(Integer, ForeignKey("games.id"))
-    player_id = Column(Integer, ForeignKey("players.id"))
-
-    game = relationship("Game", back_populates="turns")
-    player = relationship("Player", back_populates="turns")
-
-class Jail(Base):
-    __tablename__ = "jail"
-    id = Column(Integer, primary_key=True)
-    player_id = Column(Integer)
-    turns_in_jail = Column(Integer, default=0)
-
-class DiceRoll(Base):
-    __tablename__ = "dice_rolls"
-    id = Column(Integer, primary_key=True)
-    player_id = Column(Integer)
-    value = Column(Integer)
+if __name__=="__main__":
+    print("Hello!What would you like to add?")
+    print("1)player")
+    print("2)property")
+    print("3)card")
+    print("4)chance card")
+    print("5)community chest card")
+    
+    choice=input("Enter one of the choices above: ")
+    if choice=="1":
+        add_player()
+    elif choice=="2":
+        add_property()
+    elif choice=="3":
+        add_card()
+    elif choice=="4":
+        add_chancecard()
+    elif choice=="5":
+        add_communitychestcard()
+    else:
+        print("Wrong input please try again")
+    
 
 
 
-Base.metadata.create_all(engine)
-print("success")
+
+
+
+
+
+
 
---
+++ tables.py
@@ -0,0 +1,86 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from engine import engine
+
+Base = declarative_base()
+
+
+class Player(Base):
+    __tablename__ = "players"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    money = Column(Integer, default=1500)
+
+    properties = relationship("Property", back_populates="owner")
+    turns = relationship("Turn", back_populates="player")
+    transactions = relationship("Transaction", back_populates="player")
+
+class Property(Base):
+    __tablename__ = "properties"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    owner_id = Column(Integer, ForeignKey("players.id"))
+
+    owner = relationship("Player", back_populates="properties")
+
+
+class Game(Base):
+    __tablename__ = "games"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    active = Column(Boolean, default=True)
+
+    turns = relationship("Turn", back_populates="game")
+
+
+
+class Card(Base):
+    __tablename__ = "cards"
+    id = Column(Integer, primary_key=True)
+    description = Column(String)
+
+class ChanceCard(Base):
+    __tablename__ = "chance_cards"
+    id = Column(Integer, primary_key=True)
+    description = Column(String)
+
+class CommunityChestCard(Base):
+    __tablename__ = "community_chest_cards"
+    id = Column(Integer, primary_key=True)
+    description = Column(String)
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer, ForeignKey("players.id"))
+    amount = Column(Integer)
+
+    player = relationship("Player", back_populates="transactions")
+
+class Turn(Base):
+    __tablename__ = "turns"
+    id = Column(Integer, primary_key=True)
+    game_id = Column(Integer, ForeignKey("games.id"))
+    player_id = Column(Integer, ForeignKey("players.id"))
+
+    game = relationship("Game", back_populates="turns")
+    player = relationship("Player", back_populates="turns")
+
+class Jail(Base):
+    __tablename__ = "jail"
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer)
+    turns_in_jail = Column(Integer, default=0)
+
+class DiceRoll(Base):
+    __tablename__ = "dice_rolls"
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer)
+    value = Column(Integer)
+
+
+
+Base.metadata.create_all(engine)
+print("success")
+
